2a0f22c60ee1ab5536da96fabfc92f0b26ddb0e9
JonatasAS <jonatas23997@live.com>
2023-12-07 23:00:57 -0300

    Struture Home

--- ignite-shop/src/pages/index.tsx
+++ ignite-shop/src/pages/index.tsx
@@ -1,5 +1,28 @@
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={488} alt=""/>
+        <footer>
+          <strong> Camisa 1 </strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={488} alt=""/>
+        <footer>
+          <strong> Camisa 2 </strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
