6feacf8d87c069dd27dbf21f654ea02b049afdbd
JonatasAS <jonatas23997@live.com>
2024-01-02 22:41:08 -0300

    carregando dados do produto

--- ignite-shop/src/pages/product/[id].tsx
+++ ignite-shop/src/pages/product/[id].tsx
@@ -1,20 +1,34 @@
-import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+import { GetStaticProps } from "next"
 import { useRouter } from "next/router"
+import Stripe from "stripe"
 
-export default function Product() {
-  const { query } = useRouter()
+import { stripe } from "@/lib/stripe"
+
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+import Image from "next/image"
+
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  }
+}
+
+export default function Product( {product } : ProductProps) {
 
   return(
     <ProductContainer>
       <ImageContainer>
-
+          <Image src={product.imageUrl} width={520} height={480} alt=""/>
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camista X</h1>
-        <span>R$ 79,90</span>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nulla nostrum saepe, eaque magni accusantium modi voluptatem ab ea. Natus nisi unde dolorem a magni, minima amet in. Odio, quis!</p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
 
         <button>
           Comprar agora
@@ -23,3 +37,29 @@ export default function Product() {
     </ProductContainer>
   )
 }
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount! / 100), 
+        description: product.description,
+        }
+      },
+      revalidate: 60 * 60 * 1, // 1 hour
+    }
+  }
