5e1ef55195d5bbc2667bc6ee22570a19509e478c
JonatasAS <jonatas23997@live.com>
2024-01-15 22:02:22 -0300

    metatags para as paginas utilizando o Head do Next

--- ignite-shop/src/pages/index.tsx
+++ ignite-shop/src/pages/index.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { HomeContainer, Product } from "@/styles/pages/home";
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
+import Head from "next/head";
 interface HomeProps {
   products: {
     id: string;
@@ -24,21 +25,27 @@ export default function Home({ products }: HomeProps) {
     }
   })
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-     {products.map(product => {
-      return (
-        <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
-          <Product  className="keen-slider__slide">
-          <Image src={product.imageUrl} width={520} height={488} alt=""/>
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
-        </Link>
-      )
-     })}
-    </HomeContainer>
+    <>
+    <Head>
+      <title>Home | Ignite Shop </title>
+    </Head>
+    
+      <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map(product => {
+        return (
+          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+            <Product  className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={488} alt=""/>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+          </Link>
+        )
+      })}
+      </HomeContainer>
+    </>
   )
 }
 
